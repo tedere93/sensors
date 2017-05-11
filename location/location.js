584759ee1df6d01a11368eb8884c5088377a7a34
tedere93 <tedere87@gmail.com>
2017-05-11 19:18:17 +0300

    Added location functions

--- location/location.js
+++ location/location.js
@@ -1,3 +1,19 @@
-function getLocation(){
-	
+function onSuccess(pos){
+	document.getElementById("position").innerHTML ="L: "+ pos.coords.latitude + " <br>" +"l: " + pos.coords.longitude +"<br>" + "accuracy: " + pos.coords.accuracy+ " <br>" + "altitude:"+ pos.coords.altitudeAccuracy;
 }
+
+function onError(err){
+	document.getElementById("position").innerHTML = err.message;
+}
+
+var params = {
+	enableHighAccuracy:true,
+	timeout:10000,
+	maximumAge:0
+};
+
+function getLocation(){
+	var geo = navigator.geolocation;
+	geo.getCurrentPosition(onSuccess,onError,params);
+}
+
