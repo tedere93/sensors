2c5359cfbb3af94fb3d0f0efd51edf6c7bbe986b
tedere93 <tedere87@gmail.com>
2017-05-11 19:49:58 +0300

    Added a static map which centers in device's location

--- location/location.js
+++ location/location.js
@@ -1,5 +1,7 @@
 function onSuccess(pos){
 	document.getElementById("position").innerHTML ="L: "+ pos.coords.latitude + " <br>" +"l: " + pos.coords.longitude +"<br>" + "accuracy: " + pos.coords.accuracy+ " <br>" + "altitude:"+ pos.coords.altitudeAccuracy;
+	var mapString = "https://maps.googleapis.com/maps/api/staticmap?"+"center="+pos.coords.latitude + "," + pos.coords.longitude+"&zoom=17&size=400x400&key=AIzaSyA508uu-N-j7GqRbgLuVaj3-HwFVfJvMtw&maptype=hybrid";
+document.getElementById("map").setAttribute("src",mapString);
 }
 
 function onError(err){
@@ -16,4 +18,5 @@ function getLocation(){
 	var geo = navigator.geolocation;
 	geo.getCurrentPosition(onSuccess,onError,params);
 }
+
 
